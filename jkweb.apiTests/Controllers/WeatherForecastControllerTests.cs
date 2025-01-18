edd22636cdc3ac82eb60d71f57f05c48f902ebe6
romeoachi <romeo.achi@inexa-ci.lan>
2025-01-18 09:59:26 +0000

    Add UnitTest

---
+++ jkweb.apiTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace jkweb.api.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void WeatherForecastControllerTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
